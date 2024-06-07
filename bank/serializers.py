b0502bfbfc4df152626265dd37c02412fb92fb68
MunarbekOsmonbekov <munarbek9@outlook.com>
2024-06-07 14:23:05 +0600

    Account, Action model/Account viewset, serializer

--- bank/serializers.py
+++ bank/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
-from bank.models import Customer
+from bank.models import Customer, Account
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        field = ('id', 'fname', 'lname',
+        fields = ('id', 'fname', 'lname',
                  'city', 'house', 'image')
         read_only_fields = ('id',)
 
     def create(self, validated_data):
         validated_data['user_id'] = self.context['request'].user.id
         return super(CustomerSerializer, self).create(validated_data)
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ('id', 'balance', 'actions')
+        read_only_fields = ('id', 'balance', 'actions')
--- bank/views.py
+++ bank/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from rest_framework import mixins
 
-from .models import Customer
-from .serializers import CustomerSerializer
+from .models import Customer, Account
+from .serializers import CustomerSerializer, AccountSerializer
 
 class CustomerList(generics.ListCreateAPIView):
     """
@@ -26,3 +28,43 @@ class CustomerList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class CustomerDetail(generics.RetrieveUpdateAPIView):
+    serializer_class = CustomerSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomerSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Customer.objects.all()
+
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+class AccountViewSet(viewsets.GenericViewSet,
+                     mixins.ListModelMixin
+                     mixins.CreateModelMixin):
+    serializer_class = AccountSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Account.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
